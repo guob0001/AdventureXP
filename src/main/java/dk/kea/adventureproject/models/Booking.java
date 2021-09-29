1b2c8f71a09feca9e6e52e80409216dedf475f53
TripPlanner <danielwes27@gmail.com>
2021-09-29 13:34:11 +0200

    Booking model

---
+++ src/main/java/dk/kea/adventureproject/models/Booking.java
@@ -0,0 +1,76 @@
+package dk.kea.adventureproject.models;
+
+import java.util.Date;
+/**
+ * @author Daniel og Hans
+ */
+
+public class Booking {
+    private int bookingId;
+    private String employeeName;
+    private String customerName;
+    private int customerTelephone;
+    private String startDate;
+    private String timeStart;
+
+    public Booking() {
+
+    }
+
+    public Booking(int bookingId, String employeeName, String customerName, int customerTelephone, String startDate, String timeStart) {
+        this.bookingId = bookingId;
+        this.employeeName = employeeName;
+        this.customerName = customerName;
+        this.customerTelephone = customerTelephone;
+        this.startDate = startDate;
+        this.timeStart = timeStart;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getCustomerTelephone() {
+        return customerTelephone;
+    }
+
+    public void setCustomerTelephone(int customerTelephone) {
+        this.customerTelephone = customerTelephone;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(String timeStart) {
+        this.timeStart = timeStart;
+    }
+}
