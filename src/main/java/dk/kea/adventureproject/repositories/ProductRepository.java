691b38b045b4ccd0f55fef3750cc3e298abd3477
ADMIN <martin.steen.c@gmail.com>
2021-10-04 14:56:40 +0200

    read product controller/service/repo

--- src/main/java/dk/kea/adventureproject/repositories/ProductRepository.java
+++ src/main/java/dk/kea/adventureproject/repositories/ProductRepository.java
@@ -1,8 +1,24 @@
 package dk.kea.adventureproject.repositories;
 
+import dk.kea.adventureproject.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+
 /**
  * @author Julius Panduro
  */
 public class ProductRepository {
+    @Autowired
+    JdbcTemplate template;
     //Template for ProductRepository
+
+    public List<Product> fetchAll(){
+        String sql = "SELECT * FROM products";
+        RowMapper<Product> rowMapper = new BeanPropertyRowMapper<>(Product.class);
+        return template.query(sql, rowMapper);
+    }
 }
